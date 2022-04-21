a5cd1b13f7101cefb43a0927228b565eddd88193
FranciscoM99 <frank11799@hotmail.com>
2022-04-21 15:14:53 -0500

    Desarrollo del test.4

--- SRC/js/test_end.js
+++ SRC/js/test_end.js
@@ -4,7 +4,6 @@ const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
 const MAX_HIGH_SCORES = 6
 
 finalScore.innerText = mostRecentScore
